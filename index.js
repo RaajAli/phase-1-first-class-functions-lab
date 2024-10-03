3bf4ec1ea75eec8f9842ee88bb93fc2031056d9c
Raaj Ali <raajrali1999@gmail.com>
2024-10-03 17:14:25 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (arrayofScubaDrivers) => {
+    const drivers = arrayofScubaDrivers.slice(0,2)
+    return drivers
+}
+
+const returnLastTwoDrivers = (arrayofScubaDrivers) => {
+    const drivers = arrayofScubaDrivers.slice(-2)
+    return drivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (multiplier) => { 
+    return (fare) => fare * multiplier
+}
+
+const fareDoubler = (fare) => {
+    const doubledFare = createFareMultiplier(2)
+    return doubledFare(fare)
+}
+
+const fareTripler = (fare) => {
+    const TripledFare = createFareMultiplier (3)
+    return TripledFare(fare)
+}
+const selectDifferentDrivers = (arrayofDrivers, slicingFunction) => {
+    return slicingFunction(arrayofDrivers)
+}
